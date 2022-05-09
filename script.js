02f5bbede58be1fe40b9c88e98af6e70216b90a0
Jesus Quintero <jquintero@kuantum.cl>
2022-05-09 16:57:50 -0400

    upload commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const openModalButtons = document.querySelectorAll('.open-modal'),
+      modal = document.querySelector('.modal'),
+      closeModalButtons = document.querySelectorAll('.close-modal');
+
+openModalButtons.forEach(openBtn => {
+  openBtn.addEventListener('click', openModal)
+});
+
+closeModalButtons.forEach(closeBtn => {
+  closeBtn.addEventListener('click', closeModal)
+});
+
+function openModal() {
+  m.style.display = 'block'
+}
+
+function closeModal() {
+  m.style.display = 'none'
+}
