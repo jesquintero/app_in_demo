60f7a99fae28b0932eef99e544a65b62e809d541
Jesus Quintero <jquintero@kuantum.cl>
2022-05-10 19:10:32 -0400

    mas manejable

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-const openModalButtons = document.querySelectorAll('.open-modal'),
-      modal = document.querySelector('.modal'),
-      closeModalButtons = document.querySelectorAll('.close-modal');
 
-openModalButtons.forEach(openBtn => {
-  openBtn.addEventListener('click', openModal)
-});
+const all_quarters = document.getElementsByClassName('main')[0]
 
-closeModalButtons.forEach(closeBtn => {
-  closeBtn.addEventListener('click', closeModal)
-});
-
-function openModal() {
-  m.style.display = 'block'
+const hide_this = (id) => {
+    let letters = 'flip-card-side-' + id // target
+    all_quarters.classList.add('hide')
+    show_one(letters)
 }
 
-function closeModal() {
-  m.style.display = 'none'
+const show_one = (s_id) => {
+    // let this_el = s_id
+    const one_q = document.getElementsByClassName(`${s_id}`)[0];
+    document.querySelector(".flip-card-content").classList.remove("hide")
+    one_q.classList.remove("hide");
+    one_q.classList.add("pointer");
+
 }
+
+function t_r_b() {
+    // document.querySelector('.flip-card-side-a').style.transform
+    //            = 'rotateY(0deg) translateZ(100px)';
+}
+
+// $("#button").on('click', function(){
+//     $(".flip-card-content").toggleClass("flip");
+// });
